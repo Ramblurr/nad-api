5e5cfdb4e4bf1ed72e77cf4536f7a28779790f9d
Casey Link <unnamedrambler@gmail.com>
2025-12-28 17:11:39 +0100

    add nix package

--- flake.nix
+++ flake.nix
@@ -4,12 +4,15 @@
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # tracks nixpkgs unstable branch
     devshell.url = "github:numtide/devshell";
     devenv.url = "https://flakehub.com/f/ramblurr/nix-devenv/*";
+    clj-nix.url = "github:jlesquembre/clj-nix";
+
   };
   outputs =
     {
       self,
       devenv,
       devshell,
+      clj-nix,
       ...
     }:
     devenv.lib.mkFlake ./. {
@@ -17,6 +20,24 @@
         devshell.overlays.default
         devenv.overlays.default
       ];
+      packages = {
+        graalvm-ce = pkgs: pkgs.graalvmPackages.graalvm-ce;
+        default =
+          pkgs:
+          clj-nix.lib.mkCljApp {
+            inherit pkgs;
+            modules = [
+              {
+                projectSrc = ./.;
+                name = "io.github.ramblurr/nad-api";
+                main-ns = "ol.nad-api";
+                nativeImage.enable = true;
+                # customJdk.enable = true;
+              }
+            ];
+          };
+      };
+
       devShell =
         pkgs:
         pkgs.devshell.mkShell {
